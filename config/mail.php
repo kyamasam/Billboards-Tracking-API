cafdd2ac76c6870b7bd734fb9950903e0e6fa625
kyamasam <samuelkyama102@gmail.com>
2019-09-10 23:09:39 +0300

    changed sending from address

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'samfastone@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'no-reply@adklout.com'),
         'name' => env('MAIL_FROM_NAME', 'Adklout'),
     ],
 
